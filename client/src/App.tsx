1186c3424db32b5ef63497f73021ebc0dd5bcd4b
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-09-19 23:57:39 +0900

    Style: Header 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import Header from 'components/common/Header/Header';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Router from 'routes/Router';
@@ -5,6 +6,7 @@ import Router from 'routes/Router';
 function App() {
 	return (
 		<BrowserRouter>
+			<Header />
 			<Routes>
 				<Route path="*" element={<Router />} />
 			</Routes>
