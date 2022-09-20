dc1eb3b1b3236867d211adbf449aad6fbabda6e4
seonnn <ssni0105@gmail.com>
2022-09-20 17:00:53 +0900

    Feat: Qeustion 페이지 생성에 따른 라우트 추가

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Main } from 'pages/Main';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Signup } from 'pages/Signup';
 import { Study, StudyDetail } from 'pages/Study';
-import Reivew from 'pages/Interveiw/Review/Review';
+import { Question, QuestionDetail, Review } from 'pages/Interveiw';
 
 const Router = () => {
 	return (
 		<React.Fragment>
 			<Routes>
-				<Route path="/" element={<Main />} />
+				<Route path="/" element={<Navigate to="/interview/question" />} />
 				<Route path="/signup" element={<Signup />} />
-				<Route path="/study-board">
+				<Route path="/mypage" element={<div>Mypage</div>} />
+				<Route path="/profile/:id" element={<div>Profile</div>} />
+				<Route path="/interview">
+					<Route path="question" element={<div>Question</div>} />
+					<Route path="question/:id" element={<div>QuestionDetail</div>} />
+					<Route path="review" element={<Review />} />
+					<Route path="review/:id" element={<div>ReviewDetail</div>} />
+				</Route>
+				<Route path="/study">
 					<Route path="" element={<Study />} />
 					<Route path=":id" element={<StudyDetail />} />
 				</Route>
-				<Route path="/review" element={<Reivew />} />
 			</Routes>
 		</React.Fragment>
 	);
