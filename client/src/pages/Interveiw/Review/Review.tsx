61fb0002d330adac73a94a0805cc031a9a05ab67
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-09-20 20:43:39 +0900

    Style: Review 상단 레이아웃 구현

--- client/src/pages/Interveiw/Review/Review.tsx
+++ client/src/pages/Interveiw/Review/Review.tsx
@@ -1,7 +1,72 @@
+import Intro from 'components/common/Intro/Intro';
+import SearchBox from 'components/common/SearchBox/SearchBox';
+import JobSorting from 'components/common/JobSorting/JobSorting';
 import React from 'react';
+import styled from 'styled-components';
+import { BsPencil } from 'react-icons/bs';
 
-const Reivew = () => {
-	return <div>Reivew</div>;
+export const StudyContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+export const SearchBar = styled.div`
+	display: flex;
+	justify-content: space-between;
+	margin-top: 1.5rem;
+
+	width: 1200px;
+
+	background-color: aliceblue;
+
+	div {
+		display: flex;
+	}
+`;
+
+export const Filtering = styled.div`
+	margin-right: 20px;
+
+	div {
+		height: 46px;
+		line-height: 46px;
+		padding: 0 10px;
+
+		&:hover {
+			border-bottom: 3px solid #000000;
+		}
+		span {
+			cursor: pointer;
+		}
+	}
+`;
+
+const Review = () => {
+	return (
+		<StudyContainer>
+			<Intro
+				title={`면접후기`}
+				content={`소중한 면접 경험을 같이 공유해 주세요.`}
+			/>
+
+			<SearchBar>
+				<div>
+					<SearchBox />
+					<JobSorting />
+				</div>
+
+				<Filtering>
+					<div>
+						<span>최신순</span>
+					</div>
+					<div>
+						<span>인기순</span>
+					</div>
+				</Filtering>
+			</SearchBar>
+		</StudyContainer>
+	);
 };
 
-export default Reivew;
+export default Review;
