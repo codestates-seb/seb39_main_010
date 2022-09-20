ddc5b9005c12c37305bd4d78cc360a94471a092b
seonnn <ssni0105@gmail.com>
2022-09-20 17:08:51 +0900

    Refactor: 불필요한 코드 삭제

--- client/src/pages/Interveiw/index.tsx
+++ client/src/pages/Interveiw/index.tsx
@@ -1,3 +1,2 @@
 export { default as Question } from './Question/Question';
-export { default as QuestionDetail } from './Question/QuestionDetail';
 export { default as Review } from './Review/Review';
