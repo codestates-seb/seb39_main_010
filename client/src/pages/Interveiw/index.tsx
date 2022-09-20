2f32deb86e3570670636a41d6e73e76068452421
seonnn <ssni0105@gmail.com>
2022-09-20 17:01:42 +0900

    Feat: Qeustion 페이지 생성

---
+++ client/src/pages/Interveiw/index.tsx
@@ -0,0 +1,3 @@
+export { default as Question } from './Question/Question';
+export { default as QuestionDetail } from './Question/QuestionDetail';
+export { default as Review } from './Review/Review';
