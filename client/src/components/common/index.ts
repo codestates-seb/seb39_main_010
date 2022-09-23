e83897380d7aff7149afef645598901dbe3a69ca
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-09-24 00:50:06 +0900

    style: index 추가

--- client/src/components/common/index.ts
+++ client/src/components/common/index.ts
@@ -1,1 +1,4 @@
-export {}
+export { default as Sorting } from './SelectBox/Sorting';
+export { default as Header } from './Header/Header';
+export { default as SearchBox } from './InputBox/SearchBox';
+export { default as Intro } from './Intro/Intro';
--- client/src/components/pages/index.ts
+++ client/src/components/pages/index.ts
@@ -1,1 +1,5 @@
-export {}
+export { default as Accordion } from './Review/Accordion';
+export { default as Reviews } from './Review/Reviews';
+export { default as Card } from './Study/Card';
+export { default as CardContent } from './Study/CardContent';
+export { default as Category } from './Study/Category';
