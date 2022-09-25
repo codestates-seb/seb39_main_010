9e0cb2373e0faeb654eb0710255c72a6282891d7
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-09-26 00:26:11 +0900

    Feat: 탭기능 추가

--- client/src/components/pages/Study/Category.tsx
+++ client/src/components/pages/Study/Category.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { CardContentAll, CardContentDone, CardContentOn } from './Cards';
+import SearchBar from './SearchBar';
 
 export const CategoryContainer = styled.div`
 	width: 1200px;
@@ -18,28 +20,44 @@ export const CategoryContainer = styled.div`
 
 		font-weight: bold;
 		color: #b1b1b1;
-
-		&:hover {
-			border-bottom: 3px solid #000000;
-			cursor: pointer;
-			color: black;
-		}
+	}
+	.submenu {
+		cursor: pointer;
+	}
+	.focused {
+		border-bottom: 3px solid #000000;
+		color: black;
 	}
 `;
 
 const Category = () => {
+	const [currentTab, setCurrentTab] = useState(0);
+	const study = [
+		{ id: 0, title: '전체', content: <CardContentAll /> },
+		{ id: 1, title: '모집중', content: <CardContentOn /> },
+		{ id: 2, title: '모집완료', content: <CardContentDone /> },
+	];
+	const selectMenuHandler = (index: number) => {
+		setCurrentTab(index);
+	};
+	const currentpage = study[currentTab].content;
+
 	return (
-		<CategoryContainer>
-			<div>
-				<span>전체</span>
-			</div>
-			<div>
-				<span>모집중</span>
-			</div>
-			<div>
-				<span>모집완료</span>
-			</div>
-		</CategoryContainer>
+		<>
+			<CategoryContainer>
+				{study.map((ele, index) => (
+					<div
+						key={index}
+						className={currentTab === index ? 'submenu focused' : 'submenu'}
+						onClick={() => selectMenuHandler(index)}
+					>
+						<span>{ele.title}</span>
+					</div>
+				))}
+			</CategoryContainer>
+			<SearchBar />
+			<div>{currentpage}</div>
+		</>
 	);
 };
 export default Category;
