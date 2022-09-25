a1ced662bd103cb006c7588e2425d4c22db18a00
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-09-26 00:23:41 +0900

    Style: index 파일 수정

--- client/src/components/common/index.ts
+++ client/src/components/common/index.ts
@@ -1,4 +1,7 @@
-export { default as Sorting } from './SelectBox/Sorting';
+export { default as SortingRound } from './SelectBox/SortingRound';
+export { default as SortingRectangle } from './SelectBox/SortingRectangle';
 export { default as Header } from './Header/Header';
-export { default as SearchBox } from './InputBox/SearchBox';
+export { default as SearchBox } from './Input/SearchBox';
+export { default as Input } from './Input/Input';
 export { default as Intro } from './Intro/Intro';
+export { default as BasicButton } from './BasicButton/BasicButton';
--- client/src/components/pages/index.ts
+++ client/src/components/pages/index.ts
@@ -1,5 +1,5 @@
 export { default as Accordion } from './Review/Accordion';
 export { default as Reviews } from './Review/Reviews';
-export { default as Card } from './Study/Card';
-export { default as CardContent } from './Study/CardContent';
 export { default as Category } from './Study/Category';
+export { default as SearchBar } from './Study/SearchBar';
+export { default as Contents } from './Study/Write/Contents';
