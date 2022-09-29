ca01104fabc24771d394979d75833f722ad993c9
x2d7751347m <x2d7751347m@gmail.com>
2022-09-30 08:31:44 +0900

    Fix for github actions

--- client/src/pages/Study/Study.tsx
+++ client/src/pages/Study/Study.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Intro } from 'components/common';
 import { Category } from 'components/pages';
