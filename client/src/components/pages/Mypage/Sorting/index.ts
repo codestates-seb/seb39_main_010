0ed662967b73ee88b2b2380f1aa9a90a9b2eba81
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-09-29 15:38:22 +0900

    Style: index 변경

---
+++ client/src/components/pages/Mypage/Sorting/index.ts
@@ -0,0 +1,4 @@
+export { default as SortingBoxL } from './SortingBoxL';
+export { default as SortingBoxR } from './SortingBoxR';
+export { default as SortingBoxW } from './SortingBoxW';
+export { default as NewPassword } from './NewPassword';
