44a7ce0bcd6aef48637b4da353a1738643febfd4
seonnn <ssni0105@gmail.com>
2022-10-03 23:43:13 +0900

    Chore: 소셜 로고 이미지 추가

---
+++ client/src/assets/images/SocialLogos/index.ts
@@ -0,0 +1,9 @@
+import googleLogo from './google.svg';
+import kakaoLogo from './kakao.svg';
+import naverLogo from './naver.svg';
+
+export const SocialLogos = {
+	googleLogo,
+	kakaoLogo,
+	naverLogo,
+};
