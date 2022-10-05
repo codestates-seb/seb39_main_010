659b3ba6411f8b46c3a3d411d018f969b55e978d
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-10-06 00:50:22 +0900

    Style: 상세페이지 댓글 작성 컴포넌트

---
+++ client/src/components/pages/Study/Details/Comments.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import styled from 'styled-components';
+import { RiSendPlaneFill } from 'react-icons/ri';
+import Comment from './Comment';
+import CommentIntro from './CommentIntro';
+const CommentsContainer = styled.div`
+	width: 1100px;
+
+	.profilepic {
+		width: 80px;
+		height: 80px;
+	}
+
+	.commentsinput {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin: 40px 0;
+
+		div:nth-child(2) {
+			display: flex;
+			align-items: center;
+			padding: 0px 36px;
+
+			width: 988px;
+			height: 80px;
+
+			background: #f5f6f8;
+
+			border-radius: 39px;
+			font-size: 18px;
+
+			form {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+
+				width: 988px;
+
+				input {
+					width: 870px;
+
+					background: none;
+					border: none;
+					outline: none;
+					font-size: 18px;
+				}
+			}
+		}
+	}
+`;
+
+const Comments = () => {
+	return (
+		<CommentsContainer>
+			<CommentIntro
+				title={`함께 스터디를 하고 싶다면 이야기를 나누어 보세요.`}
+				content={`비방글 혹은 글 내용과 상관없는 댓글을 작성할 시 삭제될 수 있습니다.`}
+			/>
+			<div className="commentsinput">
+				<div className="profilepic">사진</div>
+				<div>
+					<form>
+						<input type="text" placeholder="댓글을 남겨 보세요" />
+						<RiSendPlaneFill size={25} color="#666666" />
+					</form>
+				</div>
+			</div>
+			<Comment />
+			<Comment />
+			<Comment />
+		</CommentsContainer>
+	);
+};
+
+export default Comments;
