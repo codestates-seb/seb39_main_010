1e3e201d58fb39e0e1d919eed2893023945b47fb
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-10-06 00:35:07 +0900

    Feat: 최신순 좋아요순 정렬 기능 수정중

--- client/src/components/pages/Study/Cards/Card/CardAll.tsx
+++ client/src/components/pages/Study/Cards/Card/CardAll.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { BsFillPersonFill, BsChatLeftDots, BsSuitHeart } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 import dummy from '../../../../../assets/data/dummy.json';
 import { Bottom, CardContainer, Middle, Top } from './style';
 
 const Card = () => {
-	const sortedCard = dummy.study.sort((a, b) => {
+	const dateSortedCard = dummy.study.sort((a, b) => {
 		return new Date(b.created_at).valueOf() - new Date(a.created_at).valueOf();
 	});
+
+	// 왜 이것만 켰다 하면 바로 좋아요순 정렬이 되는건지...
+	// const likesSortedCard = dummy.study.sort((a, b) => {
+	// 	return b.likes - a.likes;
+	// });
+
+	// 만약 최신순의 버튼이 눌렸다면(=>최신순버튼이 눌린 이벤트를 가져오기) dateSortedCard로 맵을 돌리고
+	// 좋아요순의 버튼이 눌렸다면 likesSoredCard로 맵을 돌려라
+
+	// 직업 버튼 눌린것 filtering
+
+	const navigate = useNavigate();
+
 	return (
 		<>
-			{sortedCard.map((item) => (
+			{dateSortedCard.map((item) => (
 				<CardContainer key={item.id}>
 					<div className="box">
 						<Top>
@@ -18,7 +32,9 @@ const Card = () => {
 						</Top>
 						<Middle>
 							<div>
-								<h3>{item.title}</h3>
+								<h3 onClick={() => navigate(`/study/${item.id}`)}>
+									{item.title}
+								</h3>
 								<div>{item.tag}</div>
 							</div>
 							<p>{item.content}</p>
