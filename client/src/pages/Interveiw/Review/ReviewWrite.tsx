34ece93fa8e43bcdeca6429dac62aa1294c7d220
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-10-06 00:36:39 +0900

    Style: 버튼 제거

--- client/src/pages/Interveiw/Review/ReviewWrite.tsx
+++ client/src/pages/Interveiw/Review/ReviewWrite.tsx
@@ -1,4 +1,4 @@
-import { BasicButton, Intro } from 'components/common';
+import { Intro } from 'components/common';
 import { ContentsR } from 'components/pages';
 import React from 'react';
 import styled from 'styled-components';
@@ -7,9 +7,6 @@ const ReviewWriteContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	button {
-		width: 500px;
-	}
 `;
 
 const ReviewWrite = () => {
@@ -20,7 +17,6 @@ const ReviewWrite = () => {
 				content={`소중한 면접 경험을 함께 공유해 주세요`}
 			/>
 			<ContentsR />
-			<BasicButton>등록하기</BasicButton>
 		</ReviewWriteContainer>
 	);
 };
