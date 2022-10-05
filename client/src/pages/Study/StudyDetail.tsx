62556fc19c1e4b3e27027379362627d1d11f54c8
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-10-06 00:27:12 +0900

    Style: 스터디상세내용 컴포넌트

--- client/src/pages/Study/StudyDetail.tsx
+++ client/src/pages/Study/StudyDetail.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+import { BsChatLeftDots, BsSuitHeart } from 'react-icons/bs';
+import { RiSendPlaneFill } from 'react-icons/ri';
+import Title from 'components/pages/Study/Details/Title';
+import Content from 'components/pages/Study/Details/Content';
+import Comments from 'components/pages/Study/Details/Comments';
+
+const StudyDetailContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	font-size: 18px;
+
+	.profilepic {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		width: 50px;
+		height: 50px;
+
+		background-color: #d9d9d9;
+		border-radius: 100%;
+	}
+`;
 
 const StudyDetail = () => {
-	return <div>StudyDetail</div>;
+	return (
+		<StudyDetailContainer>
+			<Title />
+			<Content />
+			<Comments />
+		</StudyDetailContainer>
+	);
 };
 
 export default StudyDetail;
