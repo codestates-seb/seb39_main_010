c01f4de7e326e4d5aa002777df1bf693bf42aef5
x2d7751347m <x2d7751347m@gmail.com>
2022-10-05 22:33:10 +0900

    Fix : Fix updatemember verification rules and expose refresh token

--- DeployServer/src/main/java/com/team10/preproject/member/token/service/TokenService.java
+++ DeployServer/src/main/java/com/team10/preproject/member/token/service/TokenService.java
@@ -13,7 +13,8 @@ public class TokenService {
 
     public Token generateToken(String email) {
 
-        long accessTokenPeriod = 1000L * 6L * 10L;
+        long accessTokenPeriod = 1000L * 6L;
+//        0L * 10L;
         long refreshTokenPeriod = 1000L * 60L * 60L;
 
 
