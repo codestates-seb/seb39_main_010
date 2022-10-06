0dfc5c6e8114fd25f8a025363c6a6c524aaaafd6
seonnn <ssni0105@gmail.com>
2022-10-06 17:54:12 +0900

    Feat: headers 추가

--- client/src/apis/authApiClient.ts
+++ client/src/apis/authApiClient.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
+import { cookie } from 'utils/cookie';
 
 // header 추가 필요
 const authApiClient = axios.create({
 	baseURL: `${process.env.REACT_APP_BASE_URL}`,
+	headers: {
+		Authorization: `Bearer ${cookie.getItem('accessToken')}}`,
+		Refresh: `${cookie.getItem('refreshToken')}`,
+	},
 });
