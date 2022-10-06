f8bdb404d0d7994cd597ff1fb79f763eddba2a64
seonnn <ssni0105@gmail.com>
2022-10-06 18:08:04 +0900

    Feat: userAtom 추가

--- client/src/recoil/atom.ts
+++ client/src/recoil/atom.ts
@@ -1,4 +1,22 @@
-import { atom } from 'recoil';
+import { atom, AtomEffect } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+import { User } from 'types';
+
+type RecoilPersist<T> = {
+	persistAtom: AtomEffect<T>;
+};
 
 // loginModal
 export const loginModalAtom = atom({ key: 'loginMoalAtom', default: false });
+
+// user
+export const { persistAtom: persistUserAtom } = recoilPersist({
+	key: 'persistUserAtom',
+	storage: localStorage,
+}) as RecoilPersist<User | null>;
+
+export const userAtom = atom({
+	key: 'userAtom',
+	default: null,
+	effects_UNSTABLE: [persistUserAtom],
+});
