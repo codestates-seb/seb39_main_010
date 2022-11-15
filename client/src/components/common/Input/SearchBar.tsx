33f3280f66de8f80e4b1f66f0a0666ffc88d14dc
seonnn <ssni0105@gmail.com>
2022-11-15 10:49:04 +0900

    Rename: SearchBar 컴포넌트 이름 변경

---
+++ client/src/components/common/Input/SearchBar.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { isPropertySignature } from 'typescript';
+
+export const SearchContainer = styled.div`
+	display: flex;
+	align-items: center;
+	padding: 9px 20px;
+	gap: 10px;
+
+	width: 277px;
+	height: 46px;
+
+	background: #f9f9fc;
+	border: 1px solid #cfcfcf;
+	border-radius: 22px;
+	form {
+		display: flex;
+		align-items: center;
+		div {
+			margin-right: 5px;
+		}
+		input {
+			width: 200px;
+
+			background: none;
+			border: none;
+			outline: none;
+			color: #000000;
+			font-size: 15px;
+			line-height: 40px;
+		}
+	}
+`;
+
+const SearchBar: React.FC<{ placeholder: string }> = (props) => {
+	const [keyword, setKeyword] = useState('');
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setKeyword(event.target.value);
+	};
+
+	return (
+		<SearchContainer>
+			<form>
+				<div>
+					<AiOutlineSearch size={25} />
+				</div>
+				<input
+					type="text"
+					placeholder={props.placeholder}
+					onChange={handleChange}
+					value={keyword}
+				/>
+			</form>
+		</SearchContainer>
+	);
+};
+
+export default SearchBar;
