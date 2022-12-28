709d62c174c516016b26c995d520e1441e7c0879
seonnn <ssni0105@gmail.com>
2022-12-28 16:39:47 +0900

    refactor: getQuestionListApi 코드 분리

--- client/src/pages/Interveiw/Question/Question.tsx
+++ client/src/pages/Interveiw/Question/Question.tsx
@@ -3,27 +3,16 @@ import Banner from 'components/pages/Question/Banner';
 import styled from 'styled-components';
 import { FilterAndSearchBar } from 'components/pages';
 import QuestionCards from 'components/pages/Question/QuestionCards';
-import apiClient from 'apis/apiClient';
+import { getQuestionListApi } from 'apis/apiClient';
 import { Question } from 'components/pages/Question/QuestionCard';
 
 const QuestionPage = () => {
 	const [questionList, setQuestionList] = useState<Question[] | undefined>();
 
-	const getQuestionListApi = async () => {
-		try {
-			const response = await await apiClient.get('/api/v1/questions');
-			console.log(response);
-
-			setQuestionList(response.data.content);
-
-			return response;
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
 	useEffect(() => {
-		getQuestionListApi();
+		getQuestionListApi().then((response: Question[]) =>
+			setQuestionList(response)
+		);
 	}, []);
 
 	return (
@@ -31,8 +20,9 @@ const QuestionPage = () => {
 			<Banner />
 			<ContentContainer>
 				<FilterAndSearchBar
+					setDataList={setQuestionList}
 					placeholder="궁금한 내용을 찾아보세요."
-					navigate="/interview/question/write"
+					url="/interview/question/write"
 				/>
 				<QuestionCards questionList={questionList} />
 			</ContentContainer>
