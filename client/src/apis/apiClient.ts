f1314f885307b512b1dba589714fc9c037e218b5
ssun <ssni0105@gmail.com>
2023-01-18 11:22:11 +0900

    feat: axios timeout 추가

--- client/src/apis/apiClient.ts
+++ client/src/apis/apiClient.ts
@@ -3,6 +3,7 @@ import { LoginSubmitForm, SignupSubmitForm } from 'types';
 
 const apiClient = axios.create({
 	baseURL: `${process.env.REACT_APP_BASE_URL}`,
+	timeout: 1000,
 });
 
 apiClient.interceptors.response.use(
