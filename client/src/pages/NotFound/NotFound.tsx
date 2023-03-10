01444eeca8c24da7431b4bbb24ae18a5a031bed0
seonnn <ssni0105@gmail.com>
2023-03-10 23:31:21 +0900

    refactor: error 처리 페이지 추가에 따른 ErrorLayout 컴포넌트 분리

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -1,48 +1,15 @@
 import React from 'react';
-import { RiChatDeleteLine } from 'react-icons/ri';
-import styled from 'styled-components';
-import { theme } from 'styles/theme';
+import ErrorLayout from 'components/common/ErrorLayout/ErrorLayout';
 
 const NotFound = () => {
 	return (
-		<Container>
-			<NotFoundBox>
-				<RiChatDeleteLine size={48} />
-				<div>죄송합니다. 해당 페이지를 찾을 수 없습니다.</div>
-				<div>
-					<span>404</span> NOT FOUND
-				</div>
-			</NotFoundBox>
-		</Container>
+		<ErrorLayout>
+			<div>죄송합니다. 해당 페이지를 찾을 수 없습니다.</div>
+			<div>
+				<span>404</span> NOT FOUND
+			</div>
+		</ErrorLayout>
 	);
 };
 
 export default NotFound;
-
-const Container = styled.div`
-	width: 100%;
-	height: 80vh;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-`;
-
-const NotFoundBox = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 0.5rem;
-	border: 4px solid ${theme.colors.blueMain};
-	padding: 2rem;
-	border-radius: 1rem;
-	font-size: 1.5rem;
-	font-weight: bold;
-	color: ${theme.colors.gray800};
-
-	svg {
-		color: ${theme.colors.blueMain};
-	}
-
-	span {
-		color: ${theme.colors.blueMain};
-	}
-`;
